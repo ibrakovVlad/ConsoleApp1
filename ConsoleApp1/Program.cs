3fc1e42293015d89e4172cc29e0abb685be1525f
ofblad <ofblad@DESKTOP-MAEGUNQ>
2023-04-29 13:34:27 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorLibrary;
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("a + b = {0}", calculator.Add(a, b));
+            Console.WriteLine("a - b = {0}", calculator.Subtract(a, b));
+            Console.WriteLine("a * b = {0}", calculator.Multiply(a, b));
+            Console.WriteLine("a / b = {0}", calculator.Divide(a, b));
+        }
+    }
+}
